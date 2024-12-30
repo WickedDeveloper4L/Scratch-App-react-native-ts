0899b21202d5e3c1acd42ac31587d7f4dec729d1
Ahia Kleine Chris <chriskleine433@gmail.com>
2024-12-30 06:00:48 +0100

    added the expo-image package, starting the main screen building

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,13 +1,11 @@
 import { Link } from "expo-router";
 import { StyleSheet, Text, View } from "react-native";
+import { Image } from "expo-image";
 
 export default function Index() {
   return (
     <View style={styles.container}>
-      <Text style={styles.text}>Home Screen</Text>
-      <Link href="/about" style={styles.button}>
-        Go to About Screen
-      </Link>
+      <View style={styles.imageContainer}></View>
     </View>
   );
 }
@@ -17,11 +15,8 @@ const styles = StyleSheet.create({
     flex: 1,
     backgroundColor: "#25292e",
     alignItems: "center",
-    justifyContent: "center",
   },
-  text: { color: "#fff" },
-  button: {
-    color: "#fff",
-    fontSize: 25,
+  imageContainer: {
+    flex: 1,
   },
 });
